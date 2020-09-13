39a3f857d201ae430b96bdb02d1111ae68f7e6e4
Qing Wang <kingchin1218@gmail.com>
2020-09-13 23:02:00 +0800

    Fix

--- core/src/core/common/remote_annotation.h
+++ core/src/core/common/remote_annotation.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <utility>
 
+#include <nameof/nameof.hpp>
+
 namespace dousi {
 
 
--- core/tests/method_macro_test.cc
+++ core/tests/method_macro_test.cc
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 
-#include "../src/core/dousi.h"
+#include "core/dousi.h"
 
 #include <iostream>
 
