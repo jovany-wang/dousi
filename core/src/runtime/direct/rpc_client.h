ff672af3c4d579e713a715aa4cb1f8b4394d9a0a
BryainZhang <pgdevuser@163.com>
2020-09-21 11:44:20 +0800

    Add spdlog (#23)
    * add spdlog
    
    * add spdlog

--- core/src/runtime/direct/rpc_client.h
+++ core/src/runtime/direct/rpc_client.h
@@ -11,6 +11,8 @@ namespace dousi {
 class RpcClient {
 public:
     RpcClient(const std::string &server_address) {
+        dousi::DousiLog::StartDousiLog("/tmp/dousi/rpclient.log",
+                                       dousi::LOG_LEVEL::DEBUG, 10, 3);
         submitter_->Init(server_address);
     }
 
