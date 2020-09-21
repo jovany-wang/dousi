eae2df6a56bf14699c122dc9a1d334a0c1f87603
Qing Wang <kingchin1218@gmail.com>
2020-09-21 18:51:09 +0800

    Add single node benchmarks. (#24)
    * Add singel node benchmarks.
    
    * Fix
    
    * fix

---
+++ core/benchmarks/single_node/qps.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <atomic>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+/// TODO(qwang): Refine this by thread local.
+class Qps {
+public:
+    void increase() {
+        counter_.fetch_add(1, std::memory_order_release);
+    }
+
+    Qps() : counter_(0) {
+        thd_ = std::thread([this] {
+            while (!stop_) {
+                const auto counter = counter_.load(std::memory_order_acquire);
+                std::cout << "Current qps is " << (counter - last_counter_) << ".\n";
+                last_counter_ = counter;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+                if (counter > 10000 * 10000) {
+                    // Unlikely code path.
+                    counter_.store(0, std::memory_order_acquire);
+                }
+            }
+        });
+    }
+
+    ~Qps() {
+        stop_ = true;
+        thd_.join();
+    }
+
+private:
+    bool stop_ = false;
+    std::thread thd_;
+    std::atomic<uint32_t> counter_;
+    uint32_t last_counter_ = 0;
+};
