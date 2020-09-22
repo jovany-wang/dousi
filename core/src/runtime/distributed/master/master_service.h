7cb8e6bf78671715257eeea12b8bfc6f7d0108d5
Qing Wang <kingchin1218@gmail.com>
2020-09-22 14:58:05 +0800

    Fix test_master conccurent issue since we use a thread pool to perform requests.

--- core/src/runtime/distributed/master/master_service.h
+++ core/src/runtime/distributed/master/master_service.h
@@ -11,6 +11,7 @@ public:
     bool RegisterService(
             const std::string &service_name,
             const std::string &service_address) {
+        std::lock_guard<std::mutex> lock {endpoints_mutex_};
         endpoints_[service_name] = service_address;
         return true;
     }
@@ -18,6 +19,7 @@ public:
     // This method is used for test only. Once we enable the mock server in test,
     // we should remove this method.
     [[nodiscard]] std::unordered_map<std::string, std::string> GetAllEndpoints() const {
+        std::lock_guard<std::mutex> lock {endpoints_mutex_};
         return endpoints_;
     }
 
@@ -25,6 +27,9 @@ private:
     // The map that maps service name to its endpoint.
     // TODO(qwang): Change the 2nd parameter's type to `Endpoint`.
     std::unordered_map<std::string, std::string> endpoints_;
+
+    // The mutex that protects endpoints above.
+    mutable std::mutex endpoints_mutex_;
 };
 
 #endif
