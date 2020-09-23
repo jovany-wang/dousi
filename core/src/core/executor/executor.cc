e6e9ca896cb966a902243eabb68f8a896383b570
Qing Wang <kingchin1218@gmail.com>
2020-09-23 14:34:28 +0800

    Refine executor to make code clean. (#34)
    * Test passed.
    
    * Fix

--- core/src/core/executor/executor.cc
+++ core/src/core/executor/executor.cc
@@ -30,4 +30,57 @@ void Executor::DoAccept() {
     });
 }
 
+void Executor::LoopToWriteResponse() {
+    while (true) {
+        DousiResponse response;
+        response_queue_.WaitAndPop(&response);
+
+        std::shared_ptr<AsioStream> stream;
+        {
+            std::lock_guard<std::mutex> lock {streams_mutex_};
+            auto it = streams_.find(response.stream_id_);
+            assert(it != streams_.end());
+            stream = it->second;
+        }
+        // Note that this result is already serialized since we should know the ReturnType of it.
+        stream->Write(response.object_id_, response.result_);
+        DOUSI_LOG(INFO) << "Method invoked, result is \"" << response.result_ << "\".";
+    }
+}
+
+void Executor::PerformRequest(const DousiRequest &request) {
+    std::function<void(const std::shared_ptr<char>&, const size_t buffer_size, std::string &)> method;
+    std::string return_value_str;
+    {
+        // perform request.
+        std::lock_guard<std::mutex> lock {mutex_};
+        // TODO(qwang): This can be refined by concurrent hash map.
+        method = registered_methods_[request.method_name_];
+    }
+    method(request.buffer_ptr_, request.buffer_size_, return_value_str);
+    response_queue_.Push(DousiResponse {request.object_id_, request.stream_id_, return_value_str});
+}
+
+void Executor::LoopToPerformRequest() {
+    while (true) {
+        DousiRequest request;
+        request_queue_.WaitAndPop(&request);
+        PerformRequest(request);
+    }
+}
+
+void Executor::InvokeMethod(uint64_t stream_id, uint32_t object_id, const std::shared_ptr<char> &buffer_ptr, const size_t &buffer_size) {
+    msgpack::unpacked unpacked;
+    msgpack::unpack(unpacked, buffer_ptr.get(), buffer_size);
+    auto tuple = unpacked.get().as<std::tuple<std::string>>();
+    const auto method_name = std::get<0>(tuple);
+
+    // If there has work thread pool, post the request to request queue.
+    if (executor_options_.work_thread_num_ > 0) {
+        request_queue_.Push(DousiRequest {object_id, stream_id, buffer_ptr, buffer_size, method_name});
+    } else {
+        PerformRequest(DousiRequest {object_id, stream_id, buffer_ptr, buffer_size, method_name});
+    }
+}
+
 }
