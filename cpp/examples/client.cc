cb4c52b23f2aaf1b36b5b82c5bd3acc3dba6fcf1
Qing Wang <kingchin1218@gmail.com>
2020-10-22 22:49:37 +0800

    Fix

--- cpp/examples/client.cc
+++ cpp/examples/client.cc
@@ -9,23 +9,20 @@ int main() {
     using namespace dousi;
 
     dousi::RpcClient rpc_client {"127.0.0.1:10001"};
-
     {
-        dousi::DousiLog::StartDousiLog("tmp/dousiclient.log",
-                                       dousi::LOG_LEVEL::DEBUG, 10, 3);
         // TODO(qwang): This should refined as a general type call.
         ServiceHandle adder1 = rpc_client.GetService("Adder");
         DousiFuture<int> sum_future1 = adder1.Call(Remote(&Adder::add), 2, 3);
         const auto sum = *sum_future1.Get();
-        DOUSI_LOG(INFO) << "sum_future1 = " << sum;
+        std::cout << "sum_future1 = " << sum << std::endl;
+
 
         int sub = *adder1.Call(Remote(&Adder::sub), 100, 25).Get();
-        DOUSI_LOG(INFO) << "sub_future1 = " << sub;
+        std::cout << "sub_future1 = " << sub << std::endl;
     }
     {
         ServiceHandle echoer = rpc_client.GetService("Echoer");
         auto echoed_str = echoer.Call(Remote(&Echoer::echo), "hello world!").Get();
-        DOUSI_LOG(INFO) << "echoed_str = " << *echoed_str;
+        std::cout << "echoed_str = " << *echoed_str << std::endl;
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100 * 1000));
 }
