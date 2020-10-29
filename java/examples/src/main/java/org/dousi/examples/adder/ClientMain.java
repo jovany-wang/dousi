25ea6c372b79a04ab768042eb6a7eeb3df982301
karbon <wangkai31415926@gmail.com>
2020-10-29 17:28:41 +0800

    fix

--- java/examples/src/main/java/org/dousi/examples/adder/ClientMain.java
+++ java/examples/src/main/java/org/dousi/examples/adder/ClientMain.java
@@ -2,11 +2,12 @@ package org.dousi.examples.adder;
 
 import org.dousi.client.DousiRpcClient;
 import org.dousi.client.NettyRpcClient;
+import org.dousi.common.exception.ParserAddrException;
 
 public class ClientMain {
 
-    public static void main(String[] args) throws InterruptedException {
-        DousiRpcClient rpcClient = new NettyRpcClient("127.0.0.1:10001");
+    public static void main(String[] args) throws InterruptedException, ParserAddrException {
+        DousiRpcClient rpcClient = new NettyRpcClient("127.0.0.1:10002");
         AdderService serviceStub = rpcClient.newStub(AdderService.class);
         int i = serviceStub.add(3, 10);
         int j = serviceStub.add(23, 43);
