7b6a6617d8cacda7c507ee3430b72f73ba016eb4
Qing Wang <kingchin1218@gmail.com>
2020-12-10 12:52:32 +0800

    WIP

--- java/rpc-client/src/main/java/org/dousi/client/Codec.java
+++ java/rpc-client/src/main/java/org/dousi/client/Codec.java
@@ -15,15 +15,24 @@ public class Codec {
 
     private final static String LONG_TYPE_NAME = "java.lang.Long";
 
+    private final static String DOUBLE_TYPE_NAME = "java.lang.Double";
+
     private final static String STRING_TYPE_NAME = "java.lang.String";
 
     public byte[] encodeFunc(String serviceName, String funcName, Object[] args) throws IOException {
         // assert args != nullptr.
+        if (args.length > 8) {
+            throw new RuntimeException("We don't support more than 8 arguments now.");
+        }
+
         MessageBufferPacker messagePacker = MessagePack.newDefaultBufferPacker();
-        // [serviceName, funcName, args...]
-        messagePacker.packArrayHeader(2 + args.length);
+        // [serviceName, funcName, paramTypeList(A 64-bits Long), args...]
+        messagePacker.packArrayHeader(3 + args.length);
         messagePacker.packString(serviceName);
         messagePacker.packString(funcName);
+
+        final long encodedParamTypes = computeEncodedValueOfParamTypeList(args);
+        messagePacker.packLong(encodedParamTypes);
 
         for (Object arg : args) {
             if (arg == null) {
@@ -79,6 +88,30 @@ public class Codec {
             ObjectMapper objectMapper = new ObjectMapper(new MessagePackFactory());
             objectMapper.setAnnotationIntrospector(new JsonArrayFormat());
             return objectMapper.readValue(encodedBytes, returnClz);
+        }
+    }
+
+    private static long computeEncodedValueOfParamTypeList(Object[] args) {
+        // check args.length > 8
+        for (Object arg : args) {
+            switch (arg) {
+                case INT_TYPE_NAME:
+                    messagePacker.packInt((int) arg);
+                    break;
+                case LONG_TYPE_NAME:
+                    messagePacker.packLong((long) arg);
+                    break;
+                case STRING_TYPE_NAME:
+                    messagePacker.packString((String) arg);
+                    break;
+                default:
+                    // We treat it as a user-defined class.
+                    ObjectMapper objectMapper = new ObjectMapper(new MessagePackFactory());
+                    objectMapper.setAnnotationIntrospector(new JsonArrayFormat());
+                    messagePacker.writePayload(objectMapper.writeValueAsBytes(arg));
+//                    throw new RuntimeException("Unknown type: " + argTypeName);
+            }
+
         }
     }
 }
