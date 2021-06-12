9159bdc78f64ec6f375c6ad56e85cb862815e9f6
Qing Wang <kingchin1218@gmail.com>
2021-06-12 22:31:00 +0800

    WIP

---
+++ java/new-dousi/src/main/java/org/dousi/newdousi/api/DousiRuntimeType.java
@@ -0,0 +1,7 @@
+package org.dousi.newdousi.api;
+
+public enum DousiRuntimeType {
+    NONE,
+    SERVER_RUNTIME,
+    CLIENT_RUNTIME
+}
--- java/new-dousi/src/main/java/org/dousi/newdousi/runtime/DousiRuntime.java
+++ java/new-dousi/src/main/java/org/dousi/newdousi/runtime/DousiRuntime.java
@@ -1,8 +1,13 @@
 package org.dousi.newdousi.runtime;
 
+import org.dousi.newdousi.api.DousiRuntimeType;
+
 import java.util.concurrent.CompletableFuture;
 
 public class DousiRuntime {
+
+    private DousiRuntimeType runtimeType = DousiRuntimeType.NONE;
+
 
     <ReturnType> CompletableFuture<ReturnType> asyncInvoke(String serviceName, String methodName, Object[] args) {
         return null;
